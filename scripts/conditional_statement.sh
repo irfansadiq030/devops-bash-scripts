c60470700526be0d89a26b5fefb389847143f286
Irfansadiq030 <irfansadiq030@gmail.com>
2026-02-26 22:23:36 +0500

    Update conditional_statement.sh to prompt for disk usage input instead of using a hardcoded value

--- scripts/conditional_statement.sh
+++ scripts/conditional_statement.sh
@@ -4,7 +4,8 @@
 # Date:       2026-02-26
 # Description: This script demonstrates conditional statements in Bash. It checks disk usage and provides appropriate messages based on the usage level.
 
-DISK_USAGE=50
+# DISK_USAGE=50
+read -p "Enter the current disk usage percentage: " DISK_USAGE
 
 if [ $DISK_USAGE -lt 80 ]; then
 	echo "Disk Usage is Normal and it is less than 80%"
