246e5f4fad2d40752b625c639c81f41dd79112d9
Irfansadiq030 <irfansadiq030@gmail.com>
2026-02-26 22:20:33 +0500

    Add conditional_statement.sh script to demonstrate disk usage checks

---
+++ scripts/conditional_statement.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Title:      Conditional Statement Script
+# Author:     Irfan Sadiq
+# Date:       2026-02-26
+# Description: This script demonstrates conditional statements in Bash. It checks disk usage and provides appropriate messages based on the usage level.
+
+DISK_USAGE=50
+
+if [ $DISK_USAGE -lt 80 ]; then
+	echo "Disk Usage is Normal and it is less than 80%"
+
+elif [ $DISK_USAGE -eq 80 ]; then
+	echo "Get ready! Usage is at $DISK_USAGE"
+
+else
+	echo "ALERT: Disk Usage is $DISK_USAGE%, which is dangerous!"
+fi
